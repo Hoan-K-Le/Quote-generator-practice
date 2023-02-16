d4e117d747d1efa804ac9bd5eb4d8f0d7ab21182
Hoan Le <hoanleecr@gmail.com>
2023-02-15 21:33:48 -0800

    added loader

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
 let apiQuotes = [] // So it is available to all function
 
+// Show Loading
+function loading() {
+  loader.hidden = false
+  quoteContainer.hidden = true
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false
+  loader.hidden = true
+}
+
 // Show new Quote
 function newQuote() {
+  loading()
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   // Check if Author field is blank and replace it with Unkown
   if (!quote.author) {
@@ -24,11 +38,14 @@ function newQuote() {
     quoteText.classList.remove('long-quote')
   }
 
+  // Set Quote, Hide loader
   quoteText.textContent = quote.text
+  complete()
 }
 
 // Grab the Quote's from an API
 async function getQuotes() {
+  loading()
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
 
   try {
@@ -52,3 +69,4 @@ twitterBtn.addEventListener('click', tweetQuote)
 
 // On Load
 getQuotes()
+loading()
